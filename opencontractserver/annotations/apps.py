a9a22c12830d7df828cfbbaf7887f92ae86683d2
JSv4 <scrudato@umich.edu>
2024-05-19 16:21:27 -0500

    Add annotation signal that generates vector embeddings.

--- opencontractserver/annotations/apps.py
+++ opencontractserver/annotations/apps.py
@@ -1,5 +1,10 @@
+import uuid
+
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 from django.utils.translation import gettext_lazy as _
+
+from opencontractserver.annotations.models import Annotation
 
 
 class AnnotationsConfig(AppConfig):
@@ -10,5 +15,10 @@ class AnnotationsConfig(AppConfig):
     def ready(self):
         try:
             import opencontractserver.annotations.signals  # noqa F401
+            from opencontractserver.annotations.signals import process_annot_on_create_atomic
+
+            post_save.connect(
+                process_annot_on_create_atomic, sender=Annotation, dispatch_uid=uuid.uuid4()
+            )
         except ImportError:
             pass

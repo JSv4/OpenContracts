eb989f8149a92353d3956099850503e1826f8c35
JSv4 <scrudato@umich.edu>
2024-07-22 00:20:54 -0700

    Added document indexes.

--- opencontractserver/documents/models.py
+++ opencontractserver/documents/models.py
@@ -69,6 +69,13 @@ class Document(BaseOCModel):
             ("update_document", "update document"),
             ("remove_document", "delete document"),
         )
+        indexes = [
+            django.db.models.Index(fields=['title']),
+            django.db.models.Index(fields=['page_count']),
+            django.db.models.Index(fields=['creator']),
+            django.db.models.Index(fields=['created']),
+            django.db.models.Index(fields=['modified']),
+        ]
 
     # Override save to update modified on save
     def save(self, *args, **kwargs):

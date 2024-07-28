eeb1d227796eb90dfe533deb650ca2848a74c1e1
JSv4 <scrudato@umich.edu>
2024-07-28 13:52:19 -0700

    Upgrade Django apps not compatible with Django 4.2.

--- config/wsgi.py
+++ config/wsgi.py
@@ -33,6 +33,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
+
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)

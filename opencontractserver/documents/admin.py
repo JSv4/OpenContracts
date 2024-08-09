c235f96dba58aeb384a51b669c1c6d5abbf4abeb
JSv4 <scrudato@umich.edu>
2024-08-08 22:20:50 -0700

    Added CorpusAction model and test.

--- opencontractserver/documents/admin.py
+++ opencontractserver/documents/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 
-from opencontractserver.documents.models import Document
+from opencontractserver.documents.models import Document, DocumentAnalysisRow
 
 
 @admin.register(Document)
 class DocumentAdmin(GuardedModelAdmin):
     list_display = ["id", "title", "description", "backend_lock", "user_lock"]
+
+@admin.register(DocumentAnalysisRow)
+class DocumentAnalysisRowAdmin(GuardedModelAdmin):
+    list_display = ["id", "document", "analysis", "extract", "creator"]

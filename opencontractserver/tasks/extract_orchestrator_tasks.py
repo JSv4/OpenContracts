321a205bc831de91e72e6b0f8e3c0c8aea71be16
JSv4 <scrudato@umich.edu>
2024-08-16 21:30:39 -0700

    Found test issue in test_extract_tasks.py

--- opencontractserver/tasks/extract_orchestrator_tasks.py
+++ opencontractserver/tasks/extract_orchestrator_tasks.py
@@ -38,7 +38,8 @@ def mark_extract_complete(extract_id):
 
 @shared_task
 def run_extract(
-    extract_id: Optional[str | int], doc_id: Optional[str | int], user_id: str | int
+    extract_id: Optional[str | int],
+    user_id: str | int
 ):
     logger.info(f"Run extract for extract {extract_id}")
 

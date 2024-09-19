919dfd7e8e6c0b96755d65360acea43cd37a67b1
JSv4 <scrudato@umich.edu>
2024-09-18 18:15:24 -0700

    Added number of improvements to admin views for sorting, filtering, and, crucially, actions.

--- opencontractserver/annotations/admin.py
+++ opencontractserver/annotations/admin.py
@@ -12,13 +12,16 @@ from opencontractserver.annotations.models import (
 @admin.register(Annotation)
 class AnnotationAdmin(GuardedModelAdmin):
     list_display = ["id", "page", "raw_text", "annotation_label"]
-    list_filter = ("analysis",)
-    search_fields = ("id", "raw_text")
+    search_fields = ['id', 'raw_text', 'annotation_label__text', 'document__title']
+    list_filter = ("analysis", 'page', 'structural', 'created', 'modified', 'is_public')
+    raw_id_fields = ('annotation_label', 'document', 'corpus', 'analysis', 'creator')
 
 
 @admin.register(Relationship)
 class RelationshipAdmin(GuardedModelAdmin):
     list_display = ["id", "relationship_label"]
+    raw_id_fields = (
+    'relationship_label', 'corpus', 'document', 'source_annotations', 'target_annotations', 'analyzer', 'creator')
 
 
 @admin.register(AnnotationLabel)

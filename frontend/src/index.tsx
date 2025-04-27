8ad42daca204e2457443ed9890aff6ea8f0ead2b
JSv4 <scrudato@umich.edu>
2025-04-27 00:26:56 -0500

    Updated bunch of references from old port 3000 to new 5173.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -32,7 +32,7 @@ const REACT_APP_APPLICATION_CLIENT_ID = window._env_
   ? window._env_.REACT_APP_APPLICATION_CLIENT_ID || ""
   : "";
 const REACT_APP_AUDIENCE = window._env_
-  ? window._env_.REACT_APP_AUDIENCE || "http://localhost:3000"
+  ? window._env_.REACT_APP_AUDIENCE || "http://localhost:5173"
   : "";
 const REACT_APP_API_ROOT_URL = window._env_
   ? window._env_.REACT_APP_API_ROOT_URL || "http://localhost:8000"

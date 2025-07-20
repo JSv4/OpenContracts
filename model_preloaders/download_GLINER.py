1c3806012c9fb14180aafbd11f2a954ad4715e8b
JSv4 <scrudato@umich.edu>
2025-07-20 03:11:51 -0500

    Working to decrease build times with new base images and push build images to GHCR.

--- model_preloaders/download_GLINER.py
+++ model_preloaders/download_GLINER.py
@@ -1,5 +1,6 @@
-from gliner import GLiNER
+# from gliner import GLiNER
 
-print("X X X PRE INSTALL GLINER")
-model = GLiNER.from_pretrained("urchade/gliner_base")
-print("COMPLETE!")
+# print("X X X PRE INSTALL GLINER")
+# model = GLiNER.from_pretrained("urchade/gliner_base")
+# print("COMPLETE!")
+print("SKIPPING GLINER PRELOAD")
